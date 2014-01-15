fcebeed4d9d4611b9a243f727f47115597e3fad7
ungrodtb <ungrodtb@msu.edu>
2014-01-15 12:36:09 -0500

    practice
    practice

---
+++ proj02/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  proj02
+//
+//  Created by Barend Ungrodt on 1/15/14.
+//  Copyright (c) 2014 Barend Ungrodt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
